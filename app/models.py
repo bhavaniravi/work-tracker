c9904af35f4271cdb4b8cb90434e0e59c6bc83b6
bhavaniravi <bhava0895@gmail.com>
2021-04-17 13:09:21 +0530

    work tracker iteration 1

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import logging
+
+
+db = SQLAlchemy(app)
+
+
+class WorkItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120))
+    estimated_time = db.Column(db.Integer, default=0)
+    is_done = db.Column(db.Boolean, default=0)
+
+class SubTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120))
+    work_item_id = db.Column(db.Integer, db.ForeignKey('work_item.id'), nullable=False)
